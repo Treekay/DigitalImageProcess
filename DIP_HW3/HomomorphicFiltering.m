331f2d44af8d45aef337c64cdd66e697f3204452
Treekay <823334594@qq.com>
2018-12-10 22:02:37 +0800

    re

--- DIP_HW3/HomomorphicFiltering.m
+++ DIP_HW3/HomomorphicFiltering.m
@@ -11,11 +11,7 @@ logImg = log(grayImg + 1);
 
 % 傅里叶变换
 fourierImg = fft2(logImg);
-
-P = m / 2;
-Q = n / 2;
-D0 = [100, 300, 500, 800, 1000, 1500, 2000];
-rank = 1;
+D0 = [100, 200, 300, 500, 800, 1000];
 rh = 2.0;
 rl = 0.25;
 C = 1.0;
@@ -24,16 +20,18 @@ G = zeros(m, n);
 for i = 1 : length(D0)
     G = fourierImg;
     % 生成同态滤波器
-    HomomorphicFilter = (rh-rl).*(1-exp((-C).*(((M-P).^2+(N-Q).^2)./D0(i).^2)))+rl;
+    HomomorphicFilter = (rh-rl).*(1-exp((-C).*(((M - m/2).^2+(N - n/2).^2)./D0(i).^2)))+rl;
     % 同态滤波
     G = G .* HomomorphicFilter;
-     % DFT 反变换并取实部
+     % 反DFT变换
     G = real(ifft2(G));
-    % 先取指数，再取实部，最后-1，因为求对数的时候+1
+    % 先取指数再取实部
     G=real(exp(G))-1;
     % 将图像像素值映射到 [0,255]
     G = uint8(255.*(G-min(min(G)))./(max(max(G))-min(min(G))));
-    figure;
+    % figure;
+    subplot(2, 3, i);
     imshow(G);title(sprintf('D0 = %d', D0(i)));
-    saveas(gcf, sprintf('./res/Butterworth_%d.jpg', D0(i)));
+    % saveas(gcf, sprintf('./res/Homomorphic_%d.jpg', D0(i)));
 end
+saveas(gcf, './res/Homorphic.jpg');
